adding dataset subsampling and fixing indices for saved softmax scores

wilds/examples/run_selftrain.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torchvision
 import sys
 import ast
+import random
 from collections import defaultdict
 import torch.nn.functional as F
 from tqdm import tqdm
@@ -18,6 +19,8 @@ from configs.utils import populate_defaults
 
 import subprocess
 import shlex
+
+split_dict = {'train': 0, 'val': 1, 'test': 2}
 
 
 def train(config, dataset_version='all_data_with_identities.csv'):
@@ -32,7 +35,7 @@ def train(config, dataset_version='all_data_with_identities.csv'):
     subprocess.run(shlex.split(cmd))
 
 
-def evaluate(config):
+def evaluate(config, dataset_version):
     dataset = config.dataset
     algorithm = config.algorithm
     root_dir = config.root_dir
@@ -40,7 +43,7 @@ def evaluate(config):
     n_epochs = config.n_epochs
     log_dir = config.log_dir
     cmd = f'python examples/run_expt.py --dataset={dataset} --log_dir {log_dir} --algorithm={algorithm} ' + \
-          f'--root_dir={root_dir} --frac={frac} --n_epochs={n_epochs} --eval_only'
+          f'--root_dir={root_dir} --frac={frac} --n_epochs={n_epochs} --dataset_version {dataset_version} --eval_only'
     subprocess.run(shlex.split(cmd))
 
 
@@ -63,10 +66,12 @@ def get_config():
     parser.add_argument('--version', default=None, type=str)
     parser.add_argument('--frac', type=float, default=1.0,
                         help='Convenience parameter that scales all dataset splits down to the specified fraction, for development purposes. Note that this also scales the test set down, so the reported numbers are not comparable with the full test set.')
+    parser.add_argument('--subsample_seed', type=int, default=1,
+                        help='Subsampling the dataset randomly')
 
     # Algorithm
     parser.add_argument('--self_train_threshold', type=float, default=0.8)
-    parser.add_argument('--self_train_rounds', type=int, default=2)
+    parser.add_argument('--self_train_rounds', type=int, default=3)
 
     # Optimization
     parser.add_argument('--n_epochs', type=int)
@@ -85,7 +90,7 @@ def get_config():
     return config
 
 
-dataset_version_dict = {
+full_dataset_version_dict = {
     'amazon':'reviews.csv',
     'bdd100k':'',
     'camelyon17':'metadata.csv',
@@ -100,6 +105,41 @@ dataset_version_dict = {
     'waterbirds':'metadata.csv',
     'yelp':'reviews.csv'
 }
+
+
+subsampled_dataset_version_dict = {
+    'amazon':'subsample_amazon.csv',
+    'bdd100k':'subsample_bdd100k.csv',
+    'camelyon17':'subsample_camelyon17.csv',
+    'celebA':'subsample_celebA.csv',
+    'civilcomments':'subsample_civilcomments.csv',
+    'fmow':'subsample_fmow.csv',
+    'iwildcam':'subsample_iwildcam.csv',
+    'ogbmolpcba':'subsample_ogbmolpcba.csv',
+    'poverty':'subsample_poverty.csv',
+    'py150':'subsample_py150.csv',
+    'sqf':'subsample_sqf.csv',
+    'waterbirds':'subsample_waterbirds.csv',
+    'yelp':'subsample_yelp.csv'
+}
+
+
+labeled_dataset_version_dict = {
+    'amazon':'labeled_amazon.csv',
+    'bdd100k':'labeled_bdd100k.csv',
+    'camelyon17':'labeled_camelyon17.csv',
+    'celebA':'labeled_celebA.csv',
+    'civilcomments':'labeled_civilcomments.csv',
+    'fmow':'labeled_fmow.csv',
+    'iwildcam':'labeled_iwildcam.csv',
+    'ogbmolpcba':'labeled_ogbmolpcba.csv',
+    'poverty':'labeled_poverty.csv',
+    'py150':'labeled_py150.csv',
+    'sqf':'labeled_sqf.csv',
+    'waterbirds':'labeled_waterbirds.csv',
+    'yelp':'labeled_yelp.csv'
+}
+
 
 label_key_dict = {
     'amazon':'y',
@@ -118,12 +158,72 @@ label_key_dict = {
 }
 
 
+def subsample(metadata_df, config, TRAIN_SIZE, UNLABELED_TEST_SIZE, LABELED_TEST_SIZE, VAL_SIZE):
+    subsampled_splits = []
+    labeled_splits = []
+
+    random.seed(config.subsample_seed)
+    for split in split_dict:
+        split_indices = metadata_df['split'] == split_dict[split]
+
+        if split == 'train':
+            indices = split_indices[split_indices == True]
+            indices = list(indices.keys())
+            indices = random.sample(indices, TRAIN_SIZE)
+            subsampled_splits.append(indices)
+            labeled_splits.append(indices)
+        elif split == 'test':
+            indices = split_indices[split_indices == True]
+            indices = list(indices.keys())
+            unlabeled_indices = random.sample(indices, UNLABELED_TEST_SIZE)
+            labeled_indices = [idx for idx in indices if idx not in unlabeled_indices]
+            labeled_indices = random.sample(labeled_indices, LABELED_TEST_SIZE)
+            subsampled_splits.append(unlabeled_indices)
+            labeled_splits.append(labeled_indices)
+        else:
+            indices = split_indices[split_indices == True]
+            indices = list(indices.keys())
+            indices = random.sample(indices, VAL_SIZE)
+            subsampled_splits.append(indices)
+            labeled_splits.append(indices)
+
+    # here the test data will be the unlabeled split for which we want to collect
+    # soft-max scores and pseudolabel
+    all_indices = subsampled_splits[0] + subsampled_splits[1] + subsampled_splits[2]
+    subsampled_df = metadata_df.loc[all_indices]
+    subsampled_df.to_csv(f"{config.root_dir}/{config.dataset}_v1.0/subsample_{config.dataset}.csv",
+                         index=False, header=list(subsampled_df.keys()))
+
+    # here the test data will be the held out test set we want to evaluate the self
+    # trained model on
+    all_labeled_indices = labeled_splits[0] + labeled_splits[1] + labeled_splits[2]
+    labeled_df = metadata_df.loc[all_labeled_indices]
+    labeled_df.to_csv(f"{config.root_dir}/{config.dataset}_v1.0/labeled_{config.dataset}.csv",
+                      index=False, header=list(labeled_df.keys()))
+
+
 def main():
     config = get_config()
-    dataset_version = dataset_version_dict[config.dataset]
+    dataset_version = subsampled_dataset_version_dict[config.dataset]
+    labeled_dataset = labeled_dataset_version_dict[config.dataset]
     label_key = label_key_dict[config.dataset]
     if not dataset_version or not label_key:
         assert 0, print("Not implemented.")
+
+    # SUBSAMPLE THE DATASET
+    TRAIN_SIZE = 500
+    VAL_SIZE = 500
+    UNLABELED_TEST_SIZE = 500
+    LABELED_TEST_SIZE = 500
+    full_dataset = wilds.get_dataset(
+        dataset=config.dataset,
+        version=config.version,
+        root_dir=config.root_dir,
+        download=config.download,
+        split_scheme=config.split_scheme,
+        **config.dataset_kwargs)
+    metadata_df = full_dataset._metadata_df
+    subsample(metadata_df, config, TRAIN_SIZE, UNLABELED_TEST_SIZE, LABELED_TEST_SIZE, VAL_SIZE)
 
     # self-train rounds
     for round in range(config.self_train_rounds):
@@ -133,8 +233,8 @@ def main():
         train(config, dataset_version=dataset_version)
 
         # run eval on original splits with the model from the last round
-        print("EVALUATING LAST ITERATION MODEL\n\n")
-        evaluate(config)
+        print(f"ROUND {round} EVALUATING MODEL\n\n")
+        evaluate(config, dataset_version=labeled_dataset)
 
         # load the predictions (softmax scores)
         keys = ['idx', 'scores']
@@ -163,8 +263,9 @@ def main():
             dataset_version=dataset_version,
             **config.dataset_kwargs)
         metadata_df = full_dataset._metadata_df
+
         for ind, _ in confident_indices.items():
-            assert ind in metadata_df.index.values.tolist()
+            assert ind in metadata_df.index.values.tolist(), print(metadata_df.index.values.tolist(), ind)
 
         pseudolabels = []
         splits = []
@@ -173,13 +274,13 @@ def main():
         for ind, row in tqdm(metadata_df.iterrows()):
             ids.append(ind)
             if ind in confident_indices:
-                splits.append(0)
+                splits.append(split_dict['train'])
                 pseudolabels.append(confident_indices[ind])
                 num_train_examples += 1
             else:
                 splits.append(row['split'])
                 pseudolabels.append(row[label_key])
-                if row['split'] == 0:
+                if row['split'] == split_dict['train']:
                     num_train_examples += 1
         assert len(metadata_df) == len(pseudolabels)
         assert len(metadata_df) == len(splits)
@@ -191,6 +292,10 @@ def main():
         dataset_version = f"iter{round+1}.csv"
         metadata_df.to_csv(f"{config.root_dir}/{config.dataset}_v1.0/{dataset_version}", index=False, header=list(metadata_df.keys()))
 
-# Example Command:
+        # if len(metadata_df[metadata_df['split'] == split_dict['test']]) == 0:
+        #     print("Pseudo labeled all examples! TODO, how to make it train without test examples.")
+        #     break
+
+
 if __name__=='__main__':
     main()

wilds/examples/wilds/datasets/wilds_dataset.py
@@ -68,10 +68,12 @@ class WILDSDataset:
             raise ValueError(f"Split {split} not found in dataset's split_dict.")
         split_mask = self.split_array == self.split_dict[split]
         split_idx = np.where(split_mask)[0]
+        split_df = self._metadata_df[self._metadata_df['split'] == self.split_dict[split]]
+        orig_idx = split_df.index
         if frac < 1.0:
             num_to_retain = int(np.round(float(len(split_idx)) * frac))
             split_idx = np.sort(np.random.permutation(split_idx)[:num_to_retain])
-        subset = WILDSSubset(self, split_idx, transform)
+        subset = WILDSSubset(self, split_idx, orig_idx, transform)
         return subset
 
     def check_init(self):
@@ -423,7 +425,7 @@ class WILDSDataset:
 
 
 class WILDSSubset(WILDSDataset):
-    def __init__(self, dataset, indices, transform):
+    def __init__(self, dataset, indices, orig_idx, transform):
         """
         This acts like torch.utils.data.Subset, but on WILDSDatasets.
         We pass in transform explicitly because it can potentially vary at
@@ -431,6 +433,7 @@ class WILDSSubset(WILDSDataset):
         """
         self.dataset = dataset
         self.indices = indices
+        self.orig_index = orig_idx
         inherited_attrs = ['_dataset_name', '_data_dir', '_collate',
                            '_split_scheme', '_split_dict', '_split_names',
                            '_y_size', '_n_classes',